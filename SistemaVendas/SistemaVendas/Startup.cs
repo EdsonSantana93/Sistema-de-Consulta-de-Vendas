8d60659dc027f5032de2a2179ec6567222bdc440
EdsonSantana93 <edson.fs193@gmail.com>
2020-10-29 22:29:55 -0300

    Criação da classe de geração de massa durante desenvolvimento

--- SistemaVendas/SistemaVendas/Startup.cs
+++ SistemaVendas/SistemaVendas/Startup.cs
@@ -39,14 +39,16 @@ namespace SistemaVendas
             services.AddDbContext<SistemaVendasContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SistemaVendasContext"), builder =>
                                builder.MigrationsAssembly("SistemaVendas")));
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.PopularTabelas();
             }
             else
             {
