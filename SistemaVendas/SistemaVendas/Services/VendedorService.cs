19e78f6af0c352f4bdd5c2d228d44ef65e85a3b8
EdsonSantana93 <edson.fs193@gmail.com>
2020-10-31 19:37:41 -0300

    Criação do VendedorService e página Razor Index da classe vendedores

---
+++ SistemaVendas/SistemaVendas/Services/VendedorService.cs
@@ -0,0 +1,25 @@
+﻿using SistemaVendas.Data;
+using SistemaVendas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaVendas.Services
+{
+    public class VendedorService
+    {
+        private readonly SistemaVendasContext _context;
+
+        public VendedorService(SistemaVendasContext context)
+        {
+            _context = context;
+        }
+        
+        //Metodo sincrono para Buscar lista de vendedores
+        public List<Vendedor> BuscarVendedores()
+        {
+            return _context.Vendedor.ToList();
+        }
+    }
+}
